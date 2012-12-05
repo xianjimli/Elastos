79be9a0ff317cd7923f1700bdfa75b430806297a
huangyukun <huang.yukun@kortide.com.cn>
2012-12-05 14:46:23 +0800

    Rename Console to Vanilla

---
+++ Elastos/Framework/Console/DevSamples/AppletDemo/Console/FooBarAppletConsole.cpp
@@ -0,0 +1,32 @@
+
+#include "FooBarApplet.h"
+#include <elastos/AutoPtr.h>
+#include <Elastos.Frameworks.Vanilla.Console.h>
+
+ECode OnQuittedEvent(PVoid userData, PInterface sender)
+{
+    CConsole::WriteLine("====FooBarAppletConsole -> OnQuittedEvent();");
+//    IAppletQuit::Probe(sender)->AtQuit(0);
+    CConsole::AddExitRoutine(sender,userData);
+    CFooBarApplet::RemoveQuittedCallback(sender, OnQuittedEvent, userData);
+    CConsole::RequestToFinish();
+    return NOERROR;
+}
+
+ECode ElastosMain(const BufferOf<CString>& args)
+{
+    CConsole::WriteLine("====FooBarAppletConsole -> ElastosMain();");
+    AutoPtr<IApplet> myApplet;
+    CFooBarApplet::New((IApplet**)&myApplet);
+
+    CConsole::ReleaseAtExit(myApplet);
+
+    ECode ec = CFooBarApplet::AddQuittedCallback(myApplet, OnQuittedEvent, NULL);
+    if (FAILED(ec)) return ec;
+
+    CConsole::WriteLine("====FooBarAppletConsole -> CFooBarApplet::AddQuittedCallback();");
+
+    myApplet->Start(args);
+
+    return NOERROR;
+}
