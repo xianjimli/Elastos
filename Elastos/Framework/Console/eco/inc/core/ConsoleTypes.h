1265b202ad22094bf9131c06d2dd3e183cece76d
huangyukun <huang.yukun@kortide.com.cn>
2012-12-05 15:01:09 +0800

    rename Console to Vanilla.

--- Elastos/Framework/Console/eco/inc/core/ConsoleTypes.h
+++
@@ -1,9 +0,0 @@
-
-#ifndef __CONSOLETYPES_H__
-#define __CONSOLETYPES_H__
-
-#include <elastos.h>
-
-typedef _ELASTOS Void (ELFUNCCALLTYPE *ConsoleExitRoutine)(_ELASTOS PVoid);
-
-#endif //__CONSOLETYPES_H__
--- Elastos/Framework/Console/eco/inc/utils/CallbackThreadUtils.h
+++
@@ -1,7 +0,0 @@
-#ifndef CALLBACK_THREAD_UTILS_H_
-#define CALLBACK_THREAD_UTILS_H_
-
-void *CreateCallbackThread(void);
-void StopCallbackThread(void *pAppletObj);
-
-#endif
