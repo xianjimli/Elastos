12f80cfabbd552cc8ca8da81a6e7b09e7c4f59aa
ryanyhlu <RyanYHLu@fih-foxconn.com>
2013-03-21 11:17:12 +0800

    checkin USB Host porting (UsbSettingsManager part)

--- Elastos/Framework/Droid/eco/inc/services/server/usb/UsbSettingsManager.h
+++ Elastos/Framework/Droid/eco/inc/services/server/usb/UsbSettingsManager.h
@@ -4,9 +4,172 @@
 
 #include "ext/frameworkext.h"
 #include <elastos/ElRefBase.h>
+#include <elastos/AutoPtr.h>
 
 class UsbSettingsManager : public ElRefBase
 {
+private:
+    /*
+     * This class is used to describe a USB device.
+     * When used in HashMaps all values must be specified,
+     * but wildcards can be used for any of the fields in
+     * the package meta-data.
+     */
+    class DeviceFilter : public ElRefBase
+    {
+    public:
+        DeviceFilter(
+            /* [in] */ Int32 vid,
+            /* [in] */ Int32 pid,
+            /* [in] */ Int32 clasz,
+            /* [in] */ Int32 subclass,
+            /* [in] */ Int32 protocol);
+
+        DeviceFilter(
+            /* [in] */ IUsbDevice* device);
+
+        CARAPI_(UInt32) AddRef();
+
+        CARAPI_(UInt32) Release();
+
+        CARAPI GetInterfaceID(
+            /* [in] */ IInterface *pObject,
+            /* [out] */ InterfaceID *pIID);
+
+        /*
+         * public static DeviceFilter read(XmlPullParser parser) throws XmlPullParserException, IOException {...}
+         */
+        static CARAPI Read(
+            /* [in] */ IXmlPullParser* parser,
+            /* [out] */ DeviceFilter** filter);
+
+        /*
+         * public void write(XmlSerializer serializer) throws IOException {...}
+         */
+        CARAPI Write(
+            /* [in] */ IXmlSerializer* serializer);
+
+        CARAPI_(Boolean) Matches(
+            /* [in] */ IUsbDevice* device);
+
+        CARAPI_(Boolean) Matches(
+            /* [in] */ DeviceFilter* filter);
+
+        CARAPI Equals(
+            /* [in] */ IInterface* obj,
+            /* [out] */ Boolean* result);
+
+        CARAPI HashCode(
+            /* [out] */ Int32* value);
+
+        CARAPI ToString(
+            /* [out] */ String* str);
+
+    private:
+        CARAPI_(Boolean) Matches(
+            /* [in] */ Int32 clasz,
+            /* [in] */ Int32 subclass,
+            /* [in] */ Int32 protocol);
+
+    private:
+        // USB Vendor ID (or -1 for unspecified)
+        Int32 mVendorId;
+
+        // USB Product ID (or -1 for unspecified)
+        Int32 mProductId;
+
+        // USB device or interface class (or -1 for unspecified)
+        Int32 mClass;
+
+        // USB device subclass (or -1 for unspecified)
+        Int32 mSubclass;
+
+        // USB device protocol (or -1 for unspecified)
+        Int32 mProtocol;
+    };
+
+    /*
+     * This class is used to describe a USB accessory.
+     * When used in HashMaps all values must be specified,
+     * but wildcards can be used for any of the fields in
+     * the package meta-data.
+     */
+    class AccessoryFilter : public ElRefBase
+    {
+    public:
+        AccessoryFilter(
+            /* [in] */ const String& manufacturer,
+            /* [in] */ const String& model,
+            /* [in] */ const String& ver);
+
+        AccessoryFilter(
+            /* [in] */ IUsbAccessory* accessory);
+
+        CARAPI_(UInt32) AddRef();
+
+        CARAPI_(UInt32) Release();
+
+        CARAPI GetInterfaceID(
+            /* [in] */ IInterface *pObject,
+            /* [out] */ InterfaceID *pIID);
+
+        /*
+         * public static AccessoryFilter read(XmlPullParser parser) throws XmlPullParserException, IOException {...}
+         */
+        static CARAPI Read(
+            /* [in] */ IXmlPullParser* parser,
+            /* [out] */ AccessoryFilter** filter);
+
+        /*
+         * public void write(XmlSerializer serializer)throws IOException {...}
+         */
+        CARAPI Write(
+            /* [in] */ IXmlSerializer* serializer);
+
+        CARAPI_(Boolean) Matches(
+            /* [in] */ IUsbAccessory* accessory);
+
+        CARAPI_(Boolean) Matches(
+            /* [in] */ AccessoryFilter* filter);
+
+        CARAPI Equals(
+            /* [in] */ IInterface* obj,
+            /* [out] */ Boolean* result);
+
+        CARAPI HashCode(
+            /* [out] */ Int32* value);
+
+        CARAPI ToString(
+            /* [out] */ String* str);
+
+    private:
+        // USB accessory manufacturer (or null for unspecified)
+        String mManufacturer;
+
+        // USB accessory model (or null for unspecified)
+        String mModel;
+
+        // USB accessory version (or null for unspecified)
+        String mVersion;
+    };
+
+    /*
+    private class MyPackageMonitor extends PackageMonitor {
+
+        public void onPackageAdded(String packageName, int uid) {
+            handlePackageUpdate(packageName);
+        }
+
+        public void onPackageChanged(String packageName, int uid, String[] components) {
+            handlePackageUpdate(packageName);
+        }
+
+        public void onPackageRemoved(String packageName, int uid) {
+            clearDefaults(packageName);
+        }
+    }
+    */
+
 public:
     UsbSettingsManager(
         /* [in] */ IContext* context);
@@ -31,24 +194,24 @@ public:
     CARAPI_(void) AccessoryDetached(
         /* [in] */ IUsbAccessory* accessory);
 
-    CARAPI_(Boolean) HasDevicePermission(
+    CARAPI_(Boolean) HasPermission(
         /* [in] */ IUsbDevice* device);
 
-    CARAPI_(Boolean) HasAccessoryPermission(
+    CARAPI_(Boolean) HasPermission(
         /* [in] */ IUsbAccessory* accessory);
 
-    CARAPI_(void) CheckDevicePermission(
+    CARAPI_(void) CheckPermission(
         /* [in] */ IUsbDevice* device);
 
-    CARAPI_(void) CheckAccessoryPermission(
+    CARAPI_(void) CheckPermission(
         /* [in] */ IUsbAccessory* accessory);
 
-    CARAPI_(void) RequestDevicePermission(
+    CARAPI_(void) RequestPermission(
         /* [in] */ IUsbDevice* device,
         /* [in] */ const String& packageName,
         /* [in] */ IPendingIntent* pi);
 
-    CARAPI_(void) RequestAccessoryPermission(
+    CARAPI_(void) RequestPermission(
         /* [in] */ IUsbAccessory* accessory,
         /* [in] */ const String& packageName,
         /* [in] */ IPendingIntent* pi);
@@ -80,6 +243,89 @@ public:
         /* [in] */ IPrintWriter* pw);
 
 private:
+    /*
+     * private void readPreference(XmlPullParser parser) throws XmlPullParserException, IOException {...}
+     */
+    CARAPI ReadPreference(
+        /* [in] */ IXmlPullParser* parser);
+
+    CARAPI_(void) ReadSettingsLocked();
+
+    CARAPI_(void) WriteSettingsLocked();
+
+    CARAPI_(Boolean) PackageMatchesLocked(
+        /* [in] */ IResolveInfo* info,
+        /* [in] */ const String& metaDataName,
+        /* [in] */ IUsbDevice* device,
+        /* [in] */ IUsbAccessory* accessory);
+
+    CARAPI_(ArrayOf<IResolveInfo*>*) GetDeviceMatchesLocked(
+        /* [in] */ IUsbDevice* device,
+        /* [in] */ IIntent* intent);
+
+    CARAPI_(ArrayOf<IResolveInfo*>*) GetAccessoryMatchesLocked(
+        /* [in] */ IUsbAccessory* accessory,
+        /* [in] */ IIntent* intent);
+
+    CARAPI_(void) ResolveActivity(
+        /* [in] */ IIntent* intent,
+        /* [in] */ const ArrayOf<IResolveInfo*>& matches,
+        /* [in] */ const String* defaultPackage,
+        /* [in] */ IUsbDevice* device,
+        /* [in] */ IUsbAccessory* accessory);
+
+    CARAPI_(Boolean) ClearCompatibleMatchesLocked(
+        /* [in] */ const String& packageName,
+        /* [in] */ DeviceFilter* filter);
+
+    CARAPI_(Boolean) ClearCompatibleMatchesLocked(
+        /* [in] */ const String& packageName,
+        /* [in] */ AccessoryFilter* filter);
+
+    CARAPI_(Boolean) HandlePackageUpdateLocked(
+        /* [in] */ const String& packageName,
+        /* [in] */ IActivityInfo* aInfo,
+        /* [in] */ const String& metaDataName);
+
+    CARAPI_(void) HandlePackageUpdate(
+        /* [in] */ const String& packageName);
+
+    CARAPI_(void) RequestPermissionDialog(
+        /* [in] */ IIntent* intent,
+        /* [in] */ const String& packageName,
+        /* [in] */ IPendingIntent* pi);
+
+    CARAPI_(Boolean) ClearPackageDefaultsLocked(
+        /* [in] */ const String& packageName);
+
+private:
+    static const Boolean DEBUG;
+
+    static const String TAG;
+
+    // For usb_audio card and device number
+    static const String USB_AUDIO_SOURCE_PATH;
+
+    static const String SETTINGS_FILE_PATH;
+
+    AutoPtr<IContext> mContext;
+    AutoPtr<ICapsuleManager> mPackageManager;
+
+    // Temporary mapping USB device name to list of UIDs with permissions for the device
+    //private final HashMap<String, SparseBooleanArray> mDevicePermissionMap = new HashMap<String, SparseBooleanArray>();
+
+    // Temporary mapping UsbAccessory to list of UIDs with permissions for the accessory
+    //private final HashMap<UsbAccessory, SparseBooleanArray> mAccessoryPermissionMap = new HashMap<UsbAccessory, SparseBooleanArray>();
+
+    // Maps DeviceFilter to user preferred application package
+    //private final HashMap<DeviceFilter, String> mDevicePreferenceMap = new HashMap<DeviceFilter, String>();
+
+    // Maps AccessoryFilter to user preferred application package
+    //private final HashMap<AccessoryFilter, String> mAccessoryPreferenceMap = new HashMap<AccessoryFilter, String>();
+
+    AutoPtr<IInterface> mLock;
+
+    //MyPackageMonitor mPackageMonitor = new MyPackageMonitor();;
 };
 
 #endif // __USBSETTINGSMANAGER_H__
--- Elastos/Framework/Droid/eco/src/services/server/usb/UsbSettingsManager.cpp
+++ Elastos/Framework/Droid/eco/src/services/server/usb/UsbSettingsManager.cpp
@@ -3,6 +3,187 @@
 
 using namespace Elastos;
 
+const Boolean UsbSettingsManager::DEBUG = FALSE;
+
+const String UsbSettingsManager::TAG = String("UsbSettingsManager");
+const String UsbSettingsManager::USB_AUDIO_SOURCE_PATH = String("/proc/asound/usb_audio_info");
+const String UsbSettingsManager::SETTINGS_FILE_PATH = String("/data/system/usb_device_manager.xml");
+
+UsbSettingsManager::DeviceFilter::DeviceFilter(
+    /* [in] */ Int32 vid,
+    /* [in] */ Int32 pid,
+    /* [in] */ Int32 clasz,
+    /* [in] */ Int32 subclass,
+    /* [in] */ Int32 protocol)
+{
+    // NOT IMPLEMENTED
+}
+
+UsbSettingsManager::DeviceFilter::DeviceFilter(
+    /* [in] */ IUsbDevice* device)
+{
+    // NOT IMPLEMENTED
+}
+
+UInt32 UsbSettingsManager::DeviceFilter::AddRef()
+{
+    return ElRefBase::AddRef();
+}
+
+UInt32 UsbSettingsManager::DeviceFilter::Release()
+{
+    return ElRefBase::Release();
+}
+
+ECode UsbSettingsManager::DeviceFilter::GetInterfaceID(
+    /* [in] */ IInterface *pObject,
+    /* [out] */ InterfaceID *pIID)
+{
+    return E_NOT_IMPLEMENTED;
+}
+
+ECode UsbSettingsManager::DeviceFilter::Read(
+    /* [in] */ IXmlPullParser* parser,
+    /* [out] */ DeviceFilter** filter)
+{
+    // NOT IMPLEMENTED
+    return NOERROR;
+}
+
+ECode UsbSettingsManager::DeviceFilter::Write(
+    /* [in] */ IXmlSerializer* serializer)
+{
+    // NOT IMPLEMENTED
+    return NOERROR;
+}
+
+Boolean UsbSettingsManager::DeviceFilter::Matches(
+    /* [in] */ IUsbDevice* device)
+{
+    // NOT IMPLEMENTED
+    return FALSE;
+}
+
+Boolean UsbSettingsManager::DeviceFilter::Matches(
+    /* [in] */ DeviceFilter* filter)
+{
+    // NOT IMPLEMENTED
+    return FALSE;
+}
+
+ECode UsbSettingsManager::DeviceFilter::Equals(
+    /* [in] */ IInterface* obj,
+    /* [out] */ Boolean* result)
+{
+    // NOT IMPLEMENTED
+    return NOERROR;
+}
+
+ECode UsbSettingsManager::DeviceFilter::HashCode(
+    /* [out] */ Int32* value)
+{
+    // NOT IMPLEMENTED
+    return NOERROR;
+}
+
+ECode UsbSettingsManager::DeviceFilter::ToString(
+    /* [out] */ String* str)
+{
+    // NOT IMPLEMENTED
+    return NOERROR;
+}
+
+Boolean UsbSettingsManager::DeviceFilter::Matches(
+    /* [in] */ Int32 clasz,
+    /* [in] */ Int32 subclass,
+    /* [in] */ Int32 protocol)
+{
+    // NOT IMPLEMENTED
+    return FALSE;
+}
+
+UsbSettingsManager::AccessoryFilter::AccessoryFilter(
+    /* [in] */ const String& manufacturer,
+    /* [in] */ const String& model,
+    /* [in] */ const String& ver)
+{
+    // NOT IMPLEMENTED
+}
+
+UsbSettingsManager::AccessoryFilter::AccessoryFilter(
+    /* [in] */ IUsbAccessory* accessory)
+{
+    // NOT IMPLEMENTED
+}
+
+UInt32 UsbSettingsManager::AccessoryFilter::AddRef()
+{
+    return ElRefBase::AddRef();
+}
+
+UInt32 UsbSettingsManager::AccessoryFilter::Release()
+{
+    return ElRefBase::Release();
+}
+
+ECode UsbSettingsManager::AccessoryFilter::GetInterfaceID(
+    /* [in] */ IInterface *pObject,
+    /* [out] */ InterfaceID *pIID)
+{
+    return E_NOT_IMPLEMENTED;
+}
+
+ECode UsbSettingsManager::AccessoryFilter::Read(
+    /* [in] */ IXmlPullParser* parser,
+    /* [out] */ AccessoryFilter** filter)
+{
+    // NOT IMPLEMENTED
+    return NOERROR;
+}
+
+ECode UsbSettingsManager::AccessoryFilter::Write(
+    /* [in] */ IXmlSerializer* serializer)
+{
+    // NOT IMPLEMENTED
+    return NOERROR;
+}
+
+Boolean UsbSettingsManager::AccessoryFilter::Matches(
+    /* [in] */ IUsbAccessory* accessory)
+{
+    // NOT IMPLEMENTED
+    return FALSE;
+}
+
+Boolean UsbSettingsManager::AccessoryFilter::Matches(
+    /* [in] */ AccessoryFilter* filter)
+{
+    // NOT IMPLEMENTED
+    return FALSE;
+}
+
+ECode UsbSettingsManager::AccessoryFilter::Equals(
+    /* [in] */ IInterface* obj,
+    /* [out] */ Boolean* result)
+{
+    // NOT IMPLEMENTED
+    return NOERROR;
+}
+
+ECode UsbSettingsManager::AccessoryFilter::HashCode(
+    /* [out] */ Int32* value)
+{
+    // NOT IMPLEMENTED
+    return NOERROR;
+}
+
+ECode UsbSettingsManager::AccessoryFilter::ToString(
+    /* [out] */ String* str)
+{
+    // NOT IMPLEMENTED
+    return NOERROR;
+}
+
 UsbSettingsManager::UsbSettingsManager(
     /* [in] */ IContext* context)
 {
@@ -50,33 +231,33 @@ void UsbSettingsManager::AccessoryDetached(
     // NOT IMPLEMENTED
 }
 
-Boolean UsbSettingsManager::HasDevicePermission(
+Boolean UsbSettingsManager::HasPermission(
     /* [in] */ IUsbDevice* device)
 {
     // NOT IMPLEMENTED
     return FALSE;
 }
 
-Boolean UsbSettingsManager::HasAccessoryPermission(
+Boolean UsbSettingsManager::HasPermission(
     /* [in] */ IUsbAccessory* accessory)
 {
     // NOT IMPLEMENTED
     return FALSE;
 }
 
-void UsbSettingsManager::CheckDevicePermission(
+void UsbSettingsManager::CheckPermission(
     /* [in] */ IUsbDevice* device)
 {
     // NOT IMPLEMENTED
 }
 
-void UsbSettingsManager::CheckAccessoryPermission(
+void UsbSettingsManager::CheckPermission(
     /* [in] */ IUsbAccessory* accessory)
 {
     // NOT IMPLEMENTED
 }
 
-void UsbSettingsManager::RequestDevicePermission(
+void UsbSettingsManager::RequestPermission(
     /* [in] */ IUsbDevice* device,
     /* [in] */ const String& packageName,
     /* [in] */ IPendingIntent* pi)
@@ -84,7 +265,7 @@ void UsbSettingsManager::RequestDevicePermission(
     // NOT IMPLEMENTED
 }
 
-void UsbSettingsManager::RequestAccessoryPermission(
+void UsbSettingsManager::RequestPermission(
     /* [in] */ IUsbAccessory* accessory,
     /* [in] */ const String& packageName,
     /* [in] */ IPendingIntent* pi)
@@ -138,4 +319,103 @@ void UsbSettingsManager::Dump(
     /* [in] */ IPrintWriter* pw)
 {
     // NOT IMPLEMENTED
+}
+
+ECode UsbSettingsManager::ReadPreference(
+    /* [in] */ IXmlPullParser* parser)
+{
+    // NOT IMPLEMENTED
+    return NOERROR;
+}
+
+void UsbSettingsManager::ReadSettingsLocked()
+{
+    // NOT IMPLEMENTED
+}
+
+void UsbSettingsManager::WriteSettingsLocked()
+{
+    // NOT IMPLEMENTED
+}
+
+Boolean UsbSettingsManager::PackageMatchesLocked(
+    /* [in] */ IResolveInfo* info,
+    /* [in] */ const String& metaDataName,
+    /* [in] */ IUsbDevice* device,
+    /* [in] */ IUsbAccessory* accessory)
+{
+    // NOT IMPLEMENTED
+    return FALSE;
+}
+
+ArrayOf<IResolveInfo*>* UsbSettingsManager::GetDeviceMatchesLocked(
+    /* [in] */ IUsbDevice* device,
+    /* [in] */ IIntent* intent)
+{
+    // NOT IMPLEMENTED
+    return NULL;
+}
+
+ArrayOf<IResolveInfo*>* UsbSettingsManager::GetAccessoryMatchesLocked(
+    /* [in] */ IUsbAccessory* accessory,
+    /* [in] */ IIntent* intent)
+{
+    // NOT IMPLEMENTED
+    return NULL;
+}
+
+void UsbSettingsManager::ResolveActivity(
+    /* [in] */ IIntent* intent,
+    /* [in] */ const ArrayOf<IResolveInfo*>& matches,
+    /* [in] */ const String* defaultPackage,
+    /* [in] */ IUsbDevice* device,
+    /* [in] */ IUsbAccessory* accessory)
+{
+    // NOT IMPLEMENTED
+}
+
+Boolean UsbSettingsManager::ClearCompatibleMatchesLocked(
+    /* [in] */ const String& packageName,
+    /* [in] */ DeviceFilter* filter)
+{
+    // NOT IMPLEMENTED
+    return FALSE;
+}
+
+Boolean UsbSettingsManager::ClearCompatibleMatchesLocked(
+    /* [in] */ const String& packageName,
+    /* [in] */ AccessoryFilter* filter)
+{
+    // NOT IMPLEMENTED
+    return FALSE;
+}
+
+Boolean UsbSettingsManager::HandlePackageUpdateLocked(
+    /* [in] */ const String& packageName,
+    /* [in] */ IActivityInfo* aInfo,
+    /* [in] */ const String& metaDataName)
+{
+    // NOT IMPLEMENTED
+    return FALSE;
+}
+
+void UsbSettingsManager::HandlePackageUpdate(
+    /* [in] */ const String& packageName)
+{
+    // NOT IMPLEMENTED
+}
+
+void UsbSettingsManager::RequestPermissionDialog(
+    /* [in] */ IIntent* intent,
+    /* [in] */ const String& packageName,
+    /* [in] */ IPendingIntent* pi)
+{
+    // NOT IMPLEMENTED
+}
+
+Boolean UsbSettingsManager::ClearPackageDefaultsLocked(
+    /* [in] */ const String& packageName)
+{
+    // NOT IMPLEMENTED
+    return FALSE;
 }
