9869ff2a88553dd7edb3e6e9adcfdca552a97755
mikewu <mikewu@fih-foxconn.com.cn>
2013-04-18 09:41:08 +0800

    Update CSyncManager, add some data member.

--- Elastos/Framework/Droid/eco/inc/core/content/CSyncManager.h
+++ Elastos/Framework/Droid/eco/inc/core/content/CSyncManager.h
@@ -4,10 +4,23 @@
 
 #include "_CSyncManager.h"
 #include <ext/frameworkdef.h>
+#include <Logger.h>
+#include <elastos/AutoPtr.h>
+
+#include "os/SystemProperties.h"
+
+using namespace Elastos;
+using namespace Elastos::Utility::Logging;
 
 CarClass(CSyncManager)
 {
+
 public:
+    CSyncManager();
+    ~CSyncManager();
+
+public:
+
     CARAPI GetSyncQueue(
         /* [out] */ ISyncQueue** syncQueue);
 
@@ -46,8 +59,95 @@ public:
         /* [in] */ IContext* context,
         /* [in] */ Boolean factoryTest);
 
+public:
+    AutoPtr<ISyncQueue> mSyncQueue;
+
 private:
-    // TODO: Add your private member variables here.
+
+    /** Delay a sync due to local changes this long. In milliseconds */
+    static Int64 LOCAL_SYNC_DELAY;
+
+    /**
+     * If a sync takes longer than this and the sync queue is not empty then we will
+     * cancel it and add it back to the end of the sync queue. In milliseconds.
+     */
+    static Int64 MAX_TIME_PER_SYNC;
+
+    static const String TAG;
+
+    static const Int64 SYNC_NOTIFICATION_DELAY = 30 * 1000; // 30 seconds
+
+    /**
+     * When retrying a sync for the first time use this delay. After that
+     * the retry time will double until it reached MAX_SYNC_RETRY_TIME.
+     * In milliseconds.
+     */
+    static const Int64 INITIAL_SYNC_RETRY_TIME_IN_MS = 30 * 1000; // 30 seconds
+
+    /**
+     * Default the max sync retry time to this value.
+     */
+    static const Int64 DEFAULT_MAX_SYNC_RETRY_TIME_IN_SECONDS = 60 * 60; // one hour
+
+    /**
+     * How long to wait before retrying a sync that failed due to one already being in progress.
+     */
+    static const Int64 DELAY_RETRY_SYNC_IN_PROGRESS_IN_SECONDS = 10;
+
+    /**
+     * An error notification is sent if sync of any of the providers has been failing for this long.
+     */
+    static const Int64 ERROR_NOTIFICATION_DELAY_MS = 1000 * 60 * 10; // 10 minutes
+
+    static const Int32 INITIALIZATION_UNBIND_DELAY_MS = 5000;
+
+    static const String SYNC_WAKE_LOCK;
+    static const String HANDLE_SYNC_ALARM_WAKE_LOCK;
+
+    AutoPtr<IContext> mContext;
+
+    volatile ArrayOf<IAccount*>* mAccounts; // = INITIAL_ACCOUNTS_ARRAY;
+
+//    volatile PowerManager.WakeLock mSyncWakeLock;
+//    volatile PowerManager.WakeLock mHandleAlarmWakeLock;
+
+    volatile Boolean mDataConnectionIsConnected; // = false;
+    volatile Boolean mStorageIsLow; // = false;
+
+    /*final*/ AutoPtr<INotificationManager> mNotificationMgr;
+//    AutoPtr<IAlarmManager> mAlarmService; // = null;
+
+    /*final*/ AutoPtr<ISyncStorageEngine> mSyncStorageEngine;
+
+//    AutoPtr<ActiveSyncContext> mActiveSyncContext; // = null;
+
+    // set if the sync error indicator should be reported.
+    Boolean mNeedSyncErrorNotification; // = false;
+    // set if the sync active indicator should be reported
+    Boolean mNeedSyncActiveNotification; // = false;
+
+    AutoPtr<IPendingIntent> mSyncAlarmIntent;
+
+    // Synchronized on "this". Instead of using this directly one should instead call
+    // its accessor, getConnManager().
+    AutoPtr<IConnectivityManager> mConnManagerDoNotUseDirectly;
+
+//    /*final*/ AutoPtr<ISyncAdaptersCache> mSyncAdapters;
+
+    static /*final*/ ArrayOf<IAccount*>* INITIAL_ACCOUNTS_ARRAY; // = new Account[0];
+
+    static const String ACTION_SYNC_ALARM; // = "android.content.syncmanager.SYNC_ALARM";
+
+//    /*final */ SyncHandler* mSyncHandler;
+    /*final */ AutoPtr<IHandler> mMainHandler;
+
+    volatile Boolean mBootCompleted; // = false;
+
+private:
+    static void InitStaticMembers();
+
+private:
+    static Boolean mIsStaticMemberInitialized;
 };
 
 #endif // __CSYNCMANAGER_H__
--- Elastos/Framework/Droid/eco/src/core/content/CSyncManager.cpp
+++ Elastos/Framework/Droid/eco/src/core/content/CSyncManager.cpp
@@ -1,81 +1,171 @@
-
 #include "content/CSyncManager.h"
 
+Int64 CSyncManager::LOCAL_SYNC_DELAY;
+Int64 CSyncManager::MAX_TIME_PER_SYNC;
+ArrayOf<IAccount*>* CSyncManager::INITIAL_ACCOUNTS_ARRAY;
+Boolean CSyncManager::mIsStaticMemberInitialized = FALSE;
+
+const String CSyncManager::TAG("SyncManager");
+const String CSyncManager::SYNC_WAKE_LOCK("*sync*");
+const String CSyncManager::HANDLE_SYNC_ALARM_WAKE_LOCK("SyncManagerHandleSyncAlarm");
+const String CSyncManager::ACTION_SYNC_ALARM("android.content.syncmanager.SYNC_ALARM");
+
+
+CSyncManager::CSyncManager()
+{
+    InitStaticMembers();
+    mAccounts = INITIAL_ACCOUNTS_ARRAY;
+    mDataConnectionIsConnected = FALSE;
+    mStorageIsLow = FALSE;
+    mNotificationMgr = NULL;
+//    mAlarmService = NULL;
+    mSyncStorageEngine = NULL;
+//    mActiveSyncContext = NULL;
+    mNeedSyncErrorNotification = FALSE;
+    mNeedSyncActiveNotification = FALSE;
+    mSyncAlarmIntent = NULL;
+    mConnManagerDoNotUseDirectly = NULL;
+//    mSyncAdapters = NULL;
+//    mSyncHandler = NULL;
+    mMainHandler = NULL;
+    mBootCompleted = FALSE;
+}
+
+CSyncManager::~CSyncManager()
+{
+//    if(mSyncHandler != NULL){
+//        delete mSyncHandler;
+//        mSyncHandler = NULL;
+//    }
+}
+
 ECode CSyncManager::GetSyncQueue(
-    /* [out] */ ISyncQueue** syncQueue)
+/* [out] */ISyncQueue** syncQueue)
 {
     // TODO: Add your code here
     return E_NOT_IMPLEMENTED;
 }
 
 ECode CSyncManager::GetSyncStorageEngine(
-    /* [out] */ ISyncStorageEngine** engine)
+/* [out] */ISyncStorageEngine** engine)
 {
     // TODO: Add your code here
     return E_NOT_IMPLEMENTED;
 }
 
 ECode CSyncManager::ScheduleSync(
-    /* [in] */ IAccount* requestedAccount,
-    /* [in] */ const String& requestedAuthority,
-    /* [in] */ IBundle* extras,
-    /* [in] */ Int64 delay,
-    /* [in] */ Boolean onlyThoseWithUnkownSyncableState)
+/* [in] */IAccount* requestedAccount,
+/* [in] */const String& requestedAuthority,
+/* [in] */IBundle* extras,
+/* [in] */Int64 delay,
+/* [in] */Boolean onlyThoseWithUnkownSyncableState)
 {
     // TODO: Add your code here
     return E_NOT_IMPLEMENTED;
 }
 
 ECode CSyncManager::ScheduleLocalSync(
-    /* [in] */ IAccount* account,
-    /* [in] */ const String& authority)
+/* [in] */IAccount* account,
+/* [in] */const String& authority)
 {
     // TODO: Add your code here
     return E_NOT_IMPLEMENTED;
 }
 
 ECode CSyncManager::GetSyncAdapterTypes(
-    /* [out, callee] */ ArrayOf<ISyncAdapterType *>** syncAdapterTypes)
+/* [out, callee] */ArrayOf<ISyncAdapterType *>** syncAdapterTypes)
 {
     // TODO: Add your code here
     return E_NOT_IMPLEMENTED;
 }
 
 ECode CSyncManager::CancelActiveSync(
-    /* [in] */ IAccount* account,
-    /* [in] */ const String& authority)
+/* [in] */IAccount* account,
+/* [in] */const String& authority)
 {
     // TODO: Add your code here
     return E_NOT_IMPLEMENTED;
 }
 
 ECode CSyncManager::ScheduleSyncOperation(
-    /* [in] */ ISyncOperation* syncOperation)
+/* [in] */ISyncOperation* syncOperation)
 {
     // TODO: Add your code here
     return E_NOT_IMPLEMENTED;
 }
 
 ECode CSyncManager::ClearScheduledSyncOperations(
-    /* [in] */ IAccount* account,
-    /* [in] */ const String& authority)
+/* [in] */IAccount* account,
+/* [in] */const String& authority)
 {
     // TODO: Add your code here
     return E_NOT_IMPLEMENTED;
 }
 
 ECode CSyncManager::OnAccountsUpdated(
-    /* [in] */ IObjectContainer* accounts)
+/* [in] */IObjectContainer* accounts)
 {
     // TODO: Add your code here
     return E_NOT_IMPLEMENTED;
 }
 
 ECode CSyncManager::constructor(
-    /* [in] */ IContext* context,
-    /* [in] */ Boolean factoryTest)
+/* [in] */IContext* context,
+/* [in] */Boolean factoryTest)
 {
     // TODO: Add your code here
     return E_NOT_IMPLEMENTED;
+}
+
+void CSyncManager::InitStaticMembers()
+{
+    if(mIsStaticMemberInitialized == TRUE){
+        return;
+    }
+
+    String localSyncDelayString = SystemProperties::Get("sync.local_sync_delay");
+    Int64 localSyncDelay = 30 * 1000; // 30 seconds
+    if (localSyncDelayString != NULL && !localSyncDelayString.IsEmpty())
+    {
+//        try
+//        {
+            Int64 tempSyncDelay = localSyncDelayString.ToInt64();
+            if (tempSyncDelay > 0)
+            {
+                localSyncDelay = tempSyncDelay;
+            }
+//        }
+//        catch (NumberFormatException nfe)
+//        {
+//            // ignore, use default
+//        }
+    }
+
+    LOCAL_SYNC_DELAY = localSyncDelay;
+
+    String maxTimePerSyncString = SystemProperties::Get("sync.max_time_per_sync");
+    long maxTimePerSync = 5 * 60 * 1000; // 5 minutes
+
+    if (maxTimePerSyncString != NULL && !maxTimePerSyncString.IsEmpty())
+    {
+//        try
+//        {
+            Int64 tempTimePerSync = maxTimePerSyncString.ToInt64();
+            if (tempTimePerSync > 0)
+            {
+                maxTimePerSync = tempTimePerSync;
+            }
+//        }
+//        catch (NumberFormatException nfe)
+//        {
+//            // ignore, use default
+//        }
+    }
+
+    MAX_TIME_PER_SYNC = maxTimePerSync;
+
+    INITIAL_ACCOUNTS_ARRAY = ArrayOf<IAccount*>::Alloc(0);
+
+    mIsStaticMemberInitialized = TRUE;
 }
 
