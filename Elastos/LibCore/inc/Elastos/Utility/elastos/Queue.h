42fdc0f65bd918c922d1bc82652ccef4cd83fceb
cao.jing <cao.jing@kortide.com.cn>
2013-04-19 14:32:41 +0800

    add Queue template

---
+++ Elastos/LibCore/inc/Elastos/Utility/elastos/Queue.h
@@ -0,0 +1,20 @@
+
+#ifndef __QUEUE_H__
+#define __QUEUE_H__
+
+
+// #pragma GCC system_header
+
+// #include <bits/c++config.h>
+#include <elastos/etl_function_exception.h>
+#include <elastos/etl_algobase.h>
+#include <elastos/etl_alloc.h>
+#include <elastos/etl_construct.h>
+#include <elastos/etl_uninitialized.h>
+#include <elastos/etl_vector.h>
+#include <elastos/etl_heap.h>
+#include <elastos/etl_deque.h>
+#include <elastos/etl_function.h>
+#include <elastos/etl_queue.h>
+
+#endif //__QUEUE_H__
