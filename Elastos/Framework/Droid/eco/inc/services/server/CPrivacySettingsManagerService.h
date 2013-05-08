6f89f213c09602eecd5bbbd9cf3de90ac1253a73
liwin <LiwinLi@fih-foxconn.com.cn>
2013-05-08 04:32:17 -0700

    Integrated PrivacySettingsManagerService in framework

---
+++ Elastos/Framework/Droid/eco/inc/services/server/CPrivacySettingsManagerService.h
@@ -0,0 +1,48 @@
+
+#ifndef __CPRIVACYSETTINGSMANAGERSERVICE_H__
+#define __CPRIVACYSETTINGSMANAGERSERVICE_H__
+
+#include "_CPrivacySettingsManagerService.h"
+
+CarClass(CPrivacySettingsManagerService)
+{
+public:
+    CARAPI GetSettings(
+        /* [in] */ const String& packageName,
+        /* [in] */ Int32 uid,
+        /* [out] */ IPrivacySettings ** ppPrivacySettings);
+
+    CARAPI SaveSettings(
+        /* [in] */ IPrivacySettings * pSettings,
+        /* [out] */ Boolean * pResult);
+
+    CARAPI DeleteSettings(
+        /* [in] */ const String& packageName,
+        /* [in] */ Int32 uid,
+        /* [out] */ Boolean * pResult);
+
+    CARAPI GetVersion();
+
+    CARAPI Notification(
+        /* [in] */ const String& packageName,
+        /* [in] */ Int32 uid,
+        /* [in] */ Byte accessMode,
+        /* [in] */ const String& dataType,
+        /* [in] */ const String& output);
+
+    CARAPI RegisterObservers();
+
+    CARAPI AddObserver(
+        /* [in] */ const String& packageName);
+
+    CARAPI PurgeSettings(
+        /* [out] */ Boolean * pResult);
+
+    CARAPI constructor(
+        /* [in] */ IContext * pContext);
+
+private:
+    // TODO: Add your private member variables here.
+};
+
+#endif // __CPRIVACYSETTINGSMANAGERSERVICE_H__
---
+++ Elastos/Framework/Droid/eco/src/services/server/CPrivacySettingsManagerService.cpp
@@ -0,0 +1,72 @@
+
+#include "server/CPrivacySettingsManagerService.h"
+ECode CPrivacySettingsManagerService::GetSettings(
+    /* [in] */ const String& packageName,
+    /* [in] */ Int32 uid,
+    /* [out] */ IPrivacySettings ** ppPrivacySettings)
+{
+    // TODO: Add your code here
+    return E_NOT_IMPLEMENTED;
+}
+
+ECode CPrivacySettingsManagerService::SaveSettings(
+    /* [in] */ IPrivacySettings * pSettings,
+    /* [out] */ Boolean * pResult)
+{
+    // TODO: Add your code here
+    return E_NOT_IMPLEMENTED;
+}
+
+ECode CPrivacySettingsManagerService::DeleteSettings(
+    /* [in] */ const String& packageName,
+    /* [in] */ Int32 uid,
+    /* [out] */ Boolean * pResult)
+{
+    // TODO: Add your code here
+    return E_NOT_IMPLEMENTED;
+}
+
+ECode CPrivacySettingsManagerService::GetVersion()
+{
+    // TODO: Add your code here
+    return E_NOT_IMPLEMENTED;
+}
+
+ECode CPrivacySettingsManagerService::Notification(
+    /* [in] */ const String& packageName,
+    /* [in] */ Int32 uid,
+    /* [in] */ Byte accessMode,
+    /* [in] */ const String& dataType,
+    /* [in] */ const String& output)
+{
+    // TODO: Add your code here
+    return E_NOT_IMPLEMENTED;
+}
+
+ECode CPrivacySettingsManagerService::RegisterObservers()
+{
+    // TODO: Add your code here
+    return E_NOT_IMPLEMENTED;
+}
+
+ECode CPrivacySettingsManagerService::AddObserver(
+    /* [in] */ const String& packageName)
+{
+    // TODO: Add your code here
+    return E_NOT_IMPLEMENTED;
+}
+
+ECode CPrivacySettingsManagerService::PurgeSettings(
+    /* [out] */ Boolean * pResult)
+{
+    // TODO: Add your code here
+    return E_NOT_IMPLEMENTED;
+}
+
+ECode CPrivacySettingsManagerService::constructor(
+    /* [in] */ IContext * pContext)
+{
+    // TODO: Add your code here
+    return E_NOT_IMPLEMENTED;
+}
+
